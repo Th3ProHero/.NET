4386ad1e6d3a18b6ec389a0792e00b710580a8fd
Mauricio Bautista Flores <107211188+Th3ProHero@users.noreply.github.com>
2024-05-31 13:25:29 -0600

    Simple Variables :3
    Open on Visual Studio

---
+++ Section2/Section2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Preheat the oven");
+            Console.WriteLine("2. Mix flour");
+            Console.WriteLine("3. Add sugar and one egg");
+            Console.WriteLine("4. Knead the mix");
+            Console.WriteLine("5. Shape and place on tray");
+            Console.WriteLine("6. Cook during ten mins");
+
+        }
+    }
+}
